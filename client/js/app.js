d74913fb346bdd3b5886fb7fd13fbf9330623840
Jay Hogan <jhogan@dominiondigital.com>
2013-09-30 16:39:17 -0400

    Log stuff.

--- client/js/app.js
+++ client/js/app.js
@@ -1,4 +1,11 @@
-App = Ember.Application.create();
+App = Ember.Application.create({
+  LOG_TRANSITIONS: true,
+  LOG_BINDINGS: true,
+  LOG_VIEW_LOOKUPS: true,
+  LOG_STACKTRACE_ON_DEPRECATION: true,
+  LOG_VERSION: true,
+  debugMode: true
+});
 
 App.ApplicationAdapter = DS.RESTAdapter.extend({
   host: 'http://localhost:3000'
